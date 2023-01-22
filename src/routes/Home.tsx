752b152a5c1e1a8531d6e883ceb1aed8acb48e47
kangkyeongseo <kks_big@naver.com>
2023-01-22 19:41:12 +0900

    getCurrentPosition

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Map from "components/Map";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Home = () => {
-  return (
-    <div>
-      <Map />
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+  const [location, setLocation] = useState({ lat: 0, lon: 0 });
+  useEffect(() => {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLocation({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+        });
+        setLoading(false);
+      });
+  }, []);
+  return <div>{loading ? null : <Map location={location} />}</div>;
 };
 
 export default Home;
