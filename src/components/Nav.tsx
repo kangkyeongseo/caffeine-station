986970334be78ee4dbc5a7f086a876b54b73ad29
kangkyeongseo <kks_big@naver.com>
2023-02-27 15:44:03 +0900

    Session with Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,10 @@
+import { sessionState } from "Atom";
 import React from "react";
 import { Link } from "react-router-dom";
+import { useRecoilState } from "recoil";
 
 const Nav = () => {
+  const [session, setSession] = useRecoilState(sessionState);
   return (
     <nav>
       <ul>
@@ -12,7 +15,11 @@ const Nav = () => {
           <Link to="/search">Search</Link>
         </li>
         <li>
-          <Link to="/login">Login</Link>
+          {session.loggedIn ? (
+            <Link to="/profile">Profile</Link>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
         </li>
       </ul>
     </nav>
