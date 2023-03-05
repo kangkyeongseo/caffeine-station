44e30c87ce076347da8c6ebcf4527991430bfa99
kangkyeongseo <kks_big@naver.com>
2023-03-05 15:41:43 +0900

    Home Style

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import KakaoMap from "components/KakaoMap";
 import { locationState } from "Atom";
 import { useRecoilValue } from "recoil";
+import styled from "styled-components";
 
 export const coffeePrice = {
   low: ["메가커피", "컴포즈커피", "메머드커피", "빽다방", "더벤티"],
@@ -10,9 +11,32 @@ export const coffeePrice = {
   high: ["스타벅스", "투썸플레이스", "폴바셋", "드롭탑", "파스쿠찌"],
 };
 
+const Container = styled.div`
+  max-width: 480px;
+  margin: 0 auto;
+  margin-top: 20px;
+`;
+
+const Lists = styled.ul`
+  display: flex;
+  gap: 20px;
+  font-weight: lighter;
+  margin-left: 20px;
+`;
+
+const List = styled.li<{ clicked: number }>`
+  color: rgba(0, 0, 0, 0.9);
+  font-weight: ${(props) => props.clicked};
+`;
+
+const MapContainer = styled.div`
+  margin-top: 20px;
+`;
+
 const Home = () => {
   const location = useRecoilValue(locationState);
   const [loading, setLoading] = useState(true);
+  const [price, setPrice] = useState("low");
   const [arr, setArr] = useState(coffeePrice.low);
 
   useEffect(() => {
@@ -29,21 +53,41 @@ const Home = () => {
     setLoading(true);
     if (price === "low") {
       setArr(coffeePrice.low);
+      setPrice("low");
     } else if (price === "middle") {
       setArr(coffeePrice.middle);
+      setPrice("middle");
     } else {
       setArr(coffeePrice.high);
+      setPrice("high");
     }
   };
   return (
-    <div>
-      <ul>
-        <li onClick={() => onPriceClick("low")}>저가</li>
-        <li onClick={() => onPriceClick("middle")}>중가</li>
-        <li onClick={() => onPriceClick("high")}>고가</li>
-      </ul>
-      {!loading ? <KakaoMap arr={arr} /> : <span>Loading</span>}
-    </div>
+    <Container>
+      <Lists>
+        <List
+          onClick={() => onPriceClick("low")}
+          clicked={price === "low" ? 500 : 300}
+        >
+          저가
+        </List>
+        <List
+          onClick={() => onPriceClick("middle")}
+          clicked={price === "middle" ? 500 : 300}
+        >
+          중가
+        </List>
+        <List
+          onClick={() => onPriceClick("high")}
+          clicked={price === "high" ? 500 : 300}
+        >
+          고가
+        </List>
+      </Lists>
+      <MapContainer>
+        {!loading ? <KakaoMap arr={arr} /> : <span>Loading</span>}
+      </MapContainer>
+    </Container>
   );
 };
 
